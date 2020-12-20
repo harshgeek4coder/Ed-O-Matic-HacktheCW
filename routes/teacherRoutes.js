829a55e6d4f650060fcc3a8af34aae639cdf1868
Harsh Sharma <sharsh2222@gmail.com>
2020-12-20 18:46:59 +0530

    refactors

---
+++ routes/teacherRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const tController = require('../controllers/teacherController')
+
+const router = Router()
+
+router.post('/signup',tController.signup_post)
+router.post('/login',tController.login_post)
+router.get('/logout', tController.logout_get);
+router.get('/:id',tController.getallfiles)
+router.get('/student/:filename',tController.getfiles)
+router.post('/student/score',tController.postScore)
+router.post('/student/sendmail',tController.sendmail)
+
+
+module.exports = router;
